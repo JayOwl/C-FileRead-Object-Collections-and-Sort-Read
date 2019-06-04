92eb782689e1a3b578cb4893bc2125cc27ab8acb
Joel Benoit <joel.benoit@tsunamisolutions.com>
2019-06-03 19:35:34 -0700

    Update Assignment

---
+++ COMP2614Assign04/COMP2614Assign04/GroceryItemCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign04
+{
+    class GroceryItemCollection : List<GroceryItem>
+    {
+        private decimal totalPrice;
+        private decimal TotalPrice
+        {
+            get { return totalPrice; }
+            set { totalPrice = value; }
+        }
+
+    }  
+}
